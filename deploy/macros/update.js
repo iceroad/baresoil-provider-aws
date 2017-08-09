25b2c246919706f40c1cb6960e476b5d08f2585d
Mayank Lahiri <mayank@iceroad.io>
2017-08-08 19:22:46 -0700

    Add next step message after update

--- deploy/macros/update.js
+++ deploy/macros/update.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
+
 function update(base, args) {
   base.getCliCommand('gen-terraform').impl.call(this, base, args);
+  console.log(chalk.green.bold(
+    'All done, you can now run "baresoil-server raise-cluster".'));
 }
 
 module.exports = update;
